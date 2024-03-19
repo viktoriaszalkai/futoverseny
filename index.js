9e78c04c172dbc757b25de10ee13befce155d190
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-03-19 21:51:56 +0100

    harmadik feladat 90%

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const TABLAZAT = document.getElementById("feladat_1");
 TABLAZAT.innerHTML = letrehozTablazat(FUTOK);
 // 1. feladat
 function letrehozTablazat(FUTOK) {
-  let txt = `<table>`;
+  let txt = `<table class = "table">`;
   for (let index = 0; index < FUTOK.length; index++) {
     txt += `
-        <tr id="befutott">
+        <tr id= "befutottTR">
             <td >${FUTOK[index].nev}</td>
             <td>${FUTOK[index].nemzetiseg}</td>
             <td>${FUTOK[index].versenySzam}</td>
@@ -21,7 +21,9 @@ function letrehozTablazat(FUTOK) {
 }
 
 const OSSZESITO = document.getElementById("feladat_2");
-OSSZESITO.innerHTML += `félmaraton: ${osszesit(FUTOK).dbFel} db<br> maraton: ${osszesit(FUTOK).dbMa} db<br> 10km: ${osszesit(FUTOK).db10} db`
+OSSZESITO.innerHTML += `félmaraton: ${osszesit(FUTOK).dbFel} db<br> maraton: ${
+  osszesit(FUTOK).dbMa
+} db<br> 10km: ${osszesit(FUTOK).db10} db`;
 // 2. feladat
 function osszesit(FUTOK) {
   var dbFel = 0;
@@ -35,27 +37,40 @@ function osszesit(FUTOK) {
     } else if (FUTOK[index].versenySzam == "10 km") {
       db10 += 1;
     }
-  }return{dbFel:dbFel,dbMa:dbMa,db10:db10};
-}/* 3 VALTOZO RETURNÖLÉSE OBJEKTUMKÉNT */
+  }
+  return { dbFel: dbFel, dbMa: dbMa, db10: db10 };
+} /* 3 VALTOZO RETURNÖLÉSE OBJEKTUMKÉNT */
 
 // 3. feladat
-const BEFUTOTT = document.getElementById("feladat_3");
-const befutottEMBER = document.querySelectorAll("#befutott");
-function befutott(befutottEMBER) {
-    for (let index = 0; index < befutottEMBER.length; index++) {
-        befutottEMBER[index].addEventListener("click", kattintas);
-      }
+const BEFUTOTT = document.getElementById("feladat_3_tarolo");
+const befutoSOR = document.querySelectorAll("#befutottTR");
+const befutottTABLAZAT = document.getElementById("feladat_3");
 
-    function kattintas(event){
-        console.log(event.target.innerHTML)
+function befutott(befutoSOR) {
+  for (let index = 0; index < befutoSOR.length; index++) {
+    befutoSOR[index].addEventListener("click", kattintas);
+
+    function kattintas(event) {
+      befutoSOR[index].classList.add("befutott");
+      /* console.log(txt) */
+      let txt = `<table class = "table">
+        <tr id="befutottTR">
+            <td >${FUTOK[index].nev}</td>
+            <td>${FUTOK[index].nemzetiseg}</td>
+            <td>${FUTOK[index].versenyIdo}</td>
+        </tr>
+        </table>`;
+      
+      befutottTABLAZAT.innerHTML += txt;
+      
+    }
+  }
+  /* console.log(befutoSOR); */
 }
-}
-befutott(befutottEMBER)
+befutott(befutoSOR);
 
 // 4. feladat
-function torolEsemeny() {
-    
-}
+function torolEsemeny() {}
 
 // 5. feladat
 function ellenoriz() {}
